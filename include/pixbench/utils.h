61388a9912a4b6567101a2acc5c37455d114e5db
Ikhwan S.H <ikhwansyatricha@gmail.com>
2025-06-09 22:32:02 +0700

    Implementing Result<T, E> type along with its unit tests.

--- include/pixbench/utils.h
+++ include/pixbench/utils.h
@@ -35,4 +35,74 @@ public:
     }
 };
 
+// Result Type
+template<typename T, typename E>
+class Result {
+private:
+    bool m_ok{ false };
+    T m_ok_result;
+    E m_err_result;
+public:
+
+    Result() = default;
+    
+    Result(bool is_ok) : m_ok(is_ok)
+    { }
+
+    /*
+     * Create Ok/Success result for Result<T, E>
+     */
+    static Result<T, E> Ok(
+        T ok_result
+        ) {
+        auto result = Result<T, E>(true);
+        result.m_ok_result = ok_result;
+        return result;
+    }
+
+    /*
+     * Create Error result for type Result<T, E>
+     */
+    static Result<T, E> Err(
+        E err_result
+        ) {
+        auto result = Result<T, E>(false);
+        result.m_err_result = err_result;
+        return result;
+    }
+
+    /*
+     * Return `true` if result is not an error
+     */
+    bool isOk() {
+        return m_ok;
+    }
+
+    /*
+     * Return `true` if result is an error
+     */
+    bool isError() {
+        return !m_ok;
+    }
+
+    /**
+     * Return nullptr if the result it not Ok
+     */
+    T* getOkResult() {
+        if ( !isOk() )
+            return nullptr;
+        return &m_ok_result;
+    }
+
+    /**
+     * Return nullptr if the result it not Error
+     */
+    E* getErrResult() {
+        if ( !isError() )
+            return nullptr;
+        return &m_err_result;
+    }
+    
+};
+
 #endif
---
+++ test/pixbench/utils_result_type_test.cpp
@@ -0,0 +1,46 @@
+#include "pixbench/utils.h"
+#include <string>
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+
+class DummyDataType {
+public:
+    double data{ 0 };
+};
+
+Result<DummyDataType, std::string> dummyFunction(
+        bool success = false
+        ) {
+    if (success) {
+        DummyDataType ok_data;
+        ok_data.data = 10;
+        return Result<DummyDataType, std::string>::Ok(ok_data);
+    }
+
+    return Result<DummyDataType, std::string>::Err(
+            "dummyFunction set to fail"
+            );
+}
+
+
+TEST_CASE("utils result type test", "[utilities]") {
+
+    // 'Ok' test
+    auto result_expect_ok = dummyFunction(true);
+    
+    REQUIRE(result_expect_ok.isOk());
+    REQUIRE(!result_expect_ok.isError());
+    REQUIRE(result_expect_ok.getOkResult() != nullptr);
+    REQUIRE(result_expect_ok.getOkResult()->data == 10);
+
+    
+    // 'Err' test
+    auto result_expect_err = dummyFunction(false);
+    
+    REQUIRE(!result_expect_err.isOk());
+    REQUIRE(result_expect_err.isError());
+    REQUIRE(result_expect_err.getErrResult() != nullptr);
+    
+};
