bfd897a6066ec26391cce644a22d912c3e8a4a61
Ikhwan S.H <ikhwansyatricha@gmail.com>
2025-09-22 22:05:39 +0700

    Fix fliped up and down

--- pixbench/vector2.cpp
+++ pixbench/vector2.cpp
@@ -150,8 +150,8 @@ double Vector2::AngleBetween(Vector2 v1, Vector2 v2) {
 }
 
 
-Vector2 Vector2::UP = Vector2(0.0, 1.0);
-Vector2 Vector2::DOWN = Vector2(0.0, -1.0);
+Vector2 Vector2::UP = Vector2(0.0, -1.0);
+Vector2 Vector2::DOWN = Vector2(0.0, 1.0);
 Vector2 Vector2::LEFT = Vector2(-1.0, -0.0);
 Vector2 Vector2::RIGHT = Vector2(1.0, 0.0);
 Vector2 Vector2::ZERO = Vector2(0.0, 0.0);
